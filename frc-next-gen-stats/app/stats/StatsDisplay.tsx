3854e4d187e37f694b3c50c35b009fc0ee3c8874
Stefan Ilic <stefan.ilic314@gmail.com>
2025-12-24 10:44:09 -0600

    add basic unstyled 5 number summaryt

--- frc-next-gen-stats/app/stats/StatsDisplay.tsx
+++ frc-next-gen-stats/app/stats/StatsDisplay.tsx
@@ -104,11 +104,46 @@ export default function StatsDisplay({ team_key, matches }: Readonly<{ team_key:
 
   };
 
+  const getFiveNumberSummary = (matches: TBAMatch[]) => {
+    const scores: number[] = [];
+    matches.forEach(match => {
+      const isRedAlliance = match.alliances.red.team_keys.includes(`${team_key}`);
+      const score = isRedAlliance ? match.alliances.red.score : match.alliances.blue.score;
+      // exclude red card matches and broken matches
+      if (score > 0) {
+        scores.push(score);
+      }
+    });
+
+    // get five number summary (min, 25th percentile, median, 75th percentile, max)
+    scores.sort((a, b) => a - b);
+    const min = scores[0] || 0;
+    const max = scores[scores.length - 1] || 0;
+    const median = scores.length % 2 === 0 ? (scores[scores.length / 2 - 1] + scores[scores.length / 2]) / 2 : scores[Math.floor(scores.length / 2)];
+    const q1 = scores.length >= 4 ? (scores[Math.floor(scores.length / 4) - 1] + scores[Math.floor(scores.length / 4)]) / 2 : min;
+    const q3 = scores.length >= 4 ? (scores[Math.floor((3 * scores.length) / 4) - 1] + scores[Math.floor((3 * scores.length) / 4)]) / 2 : max;
+
+    return { min, q1, median, q3, max };
+  }
+
   return (
     <div>
     <div className="bg-sky-300 p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow mb-4 w-fit">
       <h3 className="text-lg font-semibold text-gray-800 mb-2">Average Alliance Score - Official Matches</h3>
       <p className="text-2xl font-bold text-blue-700">{getMeanScore(matches.filter(match => match.official)).toFixed(2)}</p>
+    </div>
+
+    <div className="bg-sky-300 p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow mb-8 w-fit">
+      <h3 className="text-lg font-semibold text-gray-800 mb-2">Min</h3>
+      <p className="text-2xl font-bold text-blue-700">{getFiveNumberSummary(matches.filter(match => match.official)).min}</p>
+      <h3 className="text-lg font-semibold text-gray-800 mb-2">25th Percentile</h3>
+      <p className="text-2xl font-bold text-blue-700">{getFiveNumberSummary(matches.filter(match => match.official)).q1}</p>
+      <h3 className="text-lg font-semibold text-gray-800 mb-2">Median</h3>
+      <p className="text-2xl font-bold text-blue-700">{getFiveNumberSummary(matches.filter(match => match.official)).median}</p>
+      <h3 className="text-lg font-semibold text-gray-800 mb-2">75th Percentile</h3>
+      <p className="text-2xl font-bold text-blue-700">{getFiveNumberSummary(matches.filter(match => match.official)).q3}</p>
+      <h3 className="text-lg font-semibold text-gray-800 mb-2">Max</h3>
+      <p className="text-2xl font-bold text-blue-700">{getFiveNumberSummary(matches.filter(match => match.official)).max}</p>
     </div>
 
     <div className="grid grid-cols-3 gap-4 p-4">
