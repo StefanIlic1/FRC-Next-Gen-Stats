f7f75c3233c9be41feeaa3ecca29f9e2a6418cc0
Stefan Ilic <stefan.ilic314@gmail.com>
2025-12-24 22:12:15 -0600

    add slightly temp solution

--- frc-next-gen-stats/app/stats/page.tsx
+++ frc-next-gen-stats/app/stats/page.tsx
@@ -5,9 +5,9 @@ export default function Stats() {
     <div className="min-h-screen bg-blue-950 text-white p-8">
       <div className="max-w-7xl mx-auto">
         <div className="mb-8">
-          <h1 className="text-4xl font-bold text-sky-400 mb-4">
+            <h1 className="text-4xl font-bold text-sky-400 mb-4 pt-16">
             Next Gen Team Stats
-          </h1>
+            </h1>
           <a
             className="inline-flex h-10 items-center justify-center gap-2 rounded-full bg-sky-300 px-6 text-blue-950 transition-colors hover:bg-gray-200 mb-6"
             href="/"
